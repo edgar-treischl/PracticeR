51d760c8f07fdefd09434cd6757454d0faee91d7
Edgar Treischl <77931249+edgartreischl@users.noreply.github.com>
2022-05-23 16:41:17 +0200

    add template fun

--- R/show_script.R
+++ R/show_script.R
@@ -83,6 +83,53 @@ show_chapters <- function() {
   result
 }
 
+#' Copy a Rmarkdown template.
+#'
+#' @description The function scrapes the book template from Github
+#' and opens the template as a new rmarkdown.
+#'
+#' @param file Name of script
+#'
+#' @return Rmd
+#' @export
+#'
+
+template <- function(file) {
+  author <- "edgar-treischl"
+  repository <- "Scripts_PracticeR"
+  branch <- "main"
+  gitaddress1 <- "https://raw.githubusercontent.com/"
+
+  x <- paste(gitaddress1, "/",
+             author, "/",
+             repository, "/",
+             branch, "/",
+             "Rmd/",
+             file , ".Rmd", sep ="")
+  #source_url(x)
+  response <- httr::GET(x)
+  text <- as.character(response)
+
+  test <- text == "404: Not Found"
+
+  if (test == TRUE) {
+    print("Sorry, nothing there.")
+  } else {
+    tmp <- fs::dir_create(fs::file_temp())
+    path <- paste(tmp,"/", file, ".Rmd", sep = "")
+    fs::file_create(path)
+    id <- rstudioapi::getSourceEditorContext()$id
+    rstudioapi::navigateToFile(path)
+
+    while(rstudioapi::getSourceEditorContext()$id == id){
+      next()
+      Sys.sleep(0.1) # slow down the while loop to avoid over-processing
+    }
+    id <- rstudioapi::getSourceEditorContext()$id
+    rstudioapi::insertText(text)
+  }
+}
+
 
 utils::globalVariables(c("value"))
 
