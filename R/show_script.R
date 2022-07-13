d7750f48067369da90655b41751f8fe253684ecc
edgar-treischl <edgar.treischl@fau.de>
2022-07-13 17:14:55 +0200

    add script

--- R/show_script.R
+++ R/show_script.R
@@ -47,6 +47,83 @@ show_script <- function(file) {
 }
 
 
+
+#' Get script of Practice R.
+#' @param file The chapter file name.
+#' @return The script.
+#' @export
+#'
+
+script <- function(file) {
+  validExamples <- list.files(system.file("scripts", package = "PracticeR"))
+  validExamples <- stringr::str_split_fixed(validExamples, n = 2, pattern = ".R")
+  validExamples <- validExamples[,1]
+
+  validExamplesMsg <-
+    paste0(
+      "Valid examples are: '",
+      paste(validExamples, collapse = "', '"),
+      "'")
+
+  # if an invalid example is given, throw an error
+  if (missing(file) || !nzchar(file) ||
+      !file %in% validExamples) {
+    stop(
+      'Please run `show_script()` with a valid file name as an argument.\n',
+      validExamplesMsg,
+      call. = FALSE)
+  }
+
+  author <- "edgar-treischl"
+  repository <- "Scripts_PracticeR"
+  branch <- "main"
+  gitaddress1 <- "https://raw.githubusercontent.com/"
+  #file <- "chapter2"
+  x <- paste(gitaddress1, "/",
+             author, "/",
+             repository, "/",
+             branch, "/",
+             "R/",
+             file , ".R", sep ="")
+  #source_url(x)
+  response <- try(httr::GET(x), silent=TRUE)
+  online <- class(response) == "try-error"
+
+
+  #test <- text == "404: Not Found"
+  if (online == TRUE) {
+    print("There is internet connection, go online for the lastest version.")
+    tmp <- fs::dir_create(fs::file_temp())
+    #path <- paste(tmp,"/", file, "_script.R", sep = "")
+    file_name <- paste0(file, ".R")
+    path <- system.file("scripts", file_name, package = "PracticeR")
+    fs::file_create(path)
+    id <- rstudioapi::getSourceEditorContext()$id
+    rstudioapi::navigateToFile(path)
+    # while(rstudioapi::getSourceEditorContext()$id == id){
+    #   next()
+    #   Sys.sleep(0.1) # slow down the while loop to avoid over-processing
+    # }
+    # id <- rstudioapi::getSourceEditorContext()$id
+    # rstudioapi::insertText(text)
+  } else {
+    text <- as.character(response)
+    tmp <- fs::dir_create(fs::file_temp())
+    path <- paste(tmp,"/", file, "_script.R", sep = "")
+    fs::file_create(path)
+    id <- rstudioapi::getSourceEditorContext()$id
+    rstudioapi::navigateToFile(path)
+
+    while(rstudioapi::getSourceEditorContext()$id == id){
+      next()
+      Sys.sleep(0.1) # slow down the while loop to avoid over-processing
+    }
+    id <- rstudioapi::getSourceEditorContext()$id
+    rstudioapi::insertText(text)
+  }
+}
+
+
 #' Copy the R code of Practice R chapter.
 #'
 #' @description The function get the book script from Github
