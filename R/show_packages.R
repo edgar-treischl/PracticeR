5a49baa67bdb385935b82d81f6d48e57428a48e4
edgar-treischl <edgar.treischl@fau.de>
2022-09-20 09:13:53 +0200

    add show_packages

---
+++ R/show_packages.R
@@ -0,0 +1,56 @@
+#' Returns Packages used in Practice R
+#'
+#' @description The show_packages returns a character vector with
+#' package names used in Practice R
+#' @param file Name of script
+#' @return A character vector
+#' @export
+#'
+#'
+
+show_packages <- function(file) {
+
+  validExamples <- list.files(system.file("scripts", package = "PracticeR"))
+  validExamples <- stringr::str_split_fixed(validExamples, n = 2, pattern = ".R")
+  validExamples <- validExamples[,1]
+  validExamples <- stringr::str_sort(validExamples)
+
+
+  validExamplesMsg <-
+    paste0(
+      "Please run `show_packages()` with a valid file name as an argument. Valid examples are: ",
+      paste(validExamples, collapse = "', '"),
+      "'")
+
+  # if an invalid example is given, throw an error
+  if (missing(file) || !nzchar(file) ||
+      !file %in% validExamples) {
+    cli::cli_abort(validExamplesMsg)
+  }
+
+  path <- system.file("scripts/", package = "PracticeR")
+  filepath <- paste0(path, file, ".R")
+
+  file <- readr::read_delim(filepath,
+                            delim = "\n", escape_double = FALSE,
+                            col_names = "text", comment = "#",
+                            trim_ws = TRUE, show_col_types = FALSE)
+
+
+  text <- file$text
+  text <- stringr::str_extract(text, "library\\(\\w+")
+  text <- text[!is.na(text)]
+  text <- stringr::str_extract(text, "\\(\\w+")
+  text <- stringr::str_extract(text, "\\w+")
+  package_name <- stringi::stri_unique(text)
+  package_name <- stringr::str_sort(package_name)
+  package_name
+
+}
+
+
+
+
+
+
+
