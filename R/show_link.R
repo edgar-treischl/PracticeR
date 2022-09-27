b3337b4f6ea3d9a148b19d2ace55f7b1088298ba
edgar-treischl <edgar.treischl@fau.de>
2022-09-27 18:26:53 +0200

    update links

--- R/show_link.R
+++ R/show_link.R
@@ -10,6 +10,7 @@
 #'
 
 show_link <- function(name, browse = TRUE) {
+
   df <- tidyr::tribble(
     ~title, ~shortcut, ~link,
     "R for Data Science", "r4ds", "https://r4ds.had.co.nz/",
@@ -17,9 +18,24 @@ show_link <- function(name, browse = TRUE) {
     "Fundamentals of Data Visualization", "dataviz" , "https://clauswilke.com/dataviz/",
     "bookdown: Authoring Books and ...", "bookdown", "https://bookdown.org/yihui/bookdown/",
     "Mastering Shiny", "master_shiny","https://mastering-shiny.org/",
+    "dplyr website", "dplyr", "https://dplyr.tidyverse.org/",
+    "Pro Git", "pro-git", "https://git-scm.com/book/en/v2",
+    "R Graphics Cookbook", "r_graphics", "https://r-graphics.org/",
+    "ggplot2: Elegant Graphics for Data Analysis", "ggplot2", "https://ggplot2-book.org/",
+    "Fundamentals of Data Visualization", "data_visualization", "https://socviz.co/",
+    "R Markdown Cookbook", "rmarkdown_cookbook", "https://bookdown.org/yihui/rmarkdown-cookbook/",
+    "bookdown_website", "bookdown_website", "https://bookdown.org/",
+    "ColorBrewer", "color_brewer", "https://colorbrewer2.org/",
+    "TidyTuesday", "tidy_tuesday", "https://dplyr.tidyverse.org/",
+    "ggplot2 extension", "ggplot2_ext", "https://exts.ggplot2.tidyverse.org/",
+    "What They Forgot to Teach You About R", "forgot_teach", "https://rstats.wtf/",
     "Practice R Website", "pr_website", "https://edgar-treischl.github.io/PracticeR/",
     "Practice R Webscraping HTML", "webscraping", "https://edgar-treischl.github.io/PracticeR/articles/web_only/webscraping.html"
   )
+
+  #df <- df |> dplyr::arrange(title)
+  df <- df[order(df$title),]
+
 
   if (name == "all") {
     print(df)
